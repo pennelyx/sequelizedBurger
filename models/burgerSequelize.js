684e34197ffbb1f5a30f5595f2678442410d00ec
Peng Wang <pennelyx@gmail.com>
2017-08-13 23:56:57 -0400

    inital

---
+++ models/burgerSequelize.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, Sequelize) {
+	var BurgerSequelize = sequelize.define('burgerSequelize', {
+		id: {
+			autoIcrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER
+		},
+		burger_name: {
+			type: Sequelize.STRING,
+			notEmpty: true
+		},
+		devoured: {
+			type: Sequelize.BOOLEAN,
+			defaultValue: false
+		}
+	});
+	return BurgerSequelize;
+}
